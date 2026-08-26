ef904e3c45599802e2bc2b0405f472cb49a0b965
ss6179 <sharif.sadia009@gmail.com>
2026-08-26 00:31:40 -0400

    Navbar wip setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <div className="min-h-screen overflow-x-hidden">
+      <Navbar />
+      <main>
+        <About />
+        <Illustrations />
+        <Projects />
+      </main>
+    </div>
   )
 }
 
