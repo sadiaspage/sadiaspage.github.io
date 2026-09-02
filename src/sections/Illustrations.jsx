7fb95fb84d997ee453d9da1abde9061430216abb
ss6179 <sharif.sadia009@gmail.com>
2026-09-01 23:38:42 -0400

    Update Illustrations.jsx

--- src/sections/Illustrations.jsx
+++ src/sections/Illustrations.jsx
@@ -7,30 +7,35 @@
     id: 1,
     title: "Swimming Tiger",
     alt_text: "Tiger swimming in water",
+    widthSize: "600px",
     src: "./src/assets/illustration_tigerSwim_CMYK.png"
   },
   {
     id: 2,
-    title: "Medalist",
-    alt_text: "Girl in front of a blue background with bubbles around her",
-    src: "./src/assets/illustration_medalist.png"
-  },
-  {
-    id: 3,
     title: "Lion Dance Styles",
     alt_text: "Three lions are in a circle chasing each other, with lion dance instruments and elements surrounding them",
+    widthSize: "400px",
     src: "./src/assets/illustration_threeLionStyles.png"
   },
   {
-    id: 4,
+    id: 3,
     title: "Dragonfruit",
     alt_text: "A story about two lions breaking into a wine shop, drinking all of the alcohol, and getting drunk. The Buddha, and the wine shop owner, confronts the lions.",
+    widthSize: "400px",
     src: "./src/assets/illustration_dragonfruit.png"
   },
-    {
+  {
+    id: 4,
+    title: "Medalist",
+    alt_text: "Girl in front of a blue background with bubbles around her",
+    widthSize: "350px",
+    src: "./src/assets/illustration_medalist.png"
+  },
+  {
     id: 5,
     title: "See You Next Time!",
     alt_text: "A hamster and tiger, hand in hand, run through a reel of photos, reminiscing on their friendship together.",
+    widthSize: "400px",
     src: "./src/assets/illustration_seeYouNextTime.png"
   }
 ]
@@ -38,9 +43,9 @@
 export const Illustrations = () => {
     return (
         <section>Illustrations!
-            <div class="grid grid-cols-5 gap-3">
-            {objIllustrations.map(({ id, title, alt_text, src }) => (
-                <img key={id} src={src}></img>
+            <div class="grid grid-cols-3 gap-3">
+            {objIllustrations.map(({ id, title, alt_text, widthSize, src }) => (
+                <img key={id} src={src} alt_text ={alt_text} width={widthSize}></img>
             ))}
             </div>
         </section>
